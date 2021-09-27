95f4bdfba3491c0031a981349fb3f17971378056
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2021-09-27 18:24:50 +0300

    base algorithm implemented

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Here will e encryption algorithm")
+alphabet = "abcdefjhigklmnoprstuvwxyz"
+
+message = "hello, world"
+
+encryptedMessage = ""
+
+for i in message:
+    place = alphabet.find(i)
+    newPlace = place + 1
+
+    if i in alphabet:
+        encryptedMessage += alphabet[newPlace]
+    else:
+        encryptedMessage += i
+
+print(encryptedMessage)
+#реализация самого алгоритма ->
+#реализация ввода значений из консоли
+#поддержку языка (английский \ русский)
+#рефакторинг
+#баг фикс
